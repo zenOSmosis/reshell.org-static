a6677e14d68203c4839dd43a0db85718a961df1b
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-10-16 15:31:05 -0600

    Add CheckForUpdates and ApplicationMonitor apps

--- static/js/5.785ab5da.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpreshell=this.webpackJsonpreshell||[]).push([[5],{172:function(s,e,h){}}]);
-//# sourceMappingURL=5.785ab5da.chunk.js.map
---
+++ static/js/5.d6333a97.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreshell=this.webpackJsonpreshell||[]).push([[5],{176:function(s,e,h){}}]);
+//# sourceMappingURL=5.d6333a97.chunk.js.map
