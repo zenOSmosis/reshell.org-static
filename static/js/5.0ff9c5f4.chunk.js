b31c96b75631fd5e5f52abcafb1139e22593687b
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-10-23 15:33:54 -0600

    Update "Check for Updates" handling

--- static/js/5.0ff9c5f4.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpreshell=this.webpackJsonpreshell||[]).push([[5],{219:function(s,e,h){}}]);
-//# sourceMappingURL=5.0ff9c5f4.chunk.js.map
---
+++ static/js/5.fba49e15.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreshell=this.webpackJsonpreshell||[]).push([[5],{220:function(s,e,h){}}]);
+//# sourceMappingURL=5.fba49e15.chunk.js.map
