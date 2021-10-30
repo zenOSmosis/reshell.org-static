33054c027208db6706817cd0dc9de8dd999087ec
Jeremy Harris <jeremy.harris@zenosmosis.com>
2021-10-30 15:23:13 -0600

    Implement fix for mobile scrolling

--- static/js/5.136bd7d8.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpreshell=this.webpackJsonpreshell||[]).push([[5],{228:function(s,e,h){}}]);
-//# sourceMappingURL=5.136bd7d8.chunk.js.map
---
+++ static/js/5.aba012b1.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreshell=this.webpackJsonpreshell||[]).push([[5],{231:function(s,e,h){}}]);
+//# sourceMappingURL=5.aba012b1.chunk.js.map
